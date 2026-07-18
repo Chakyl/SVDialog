a532092078cab433f0f8df40e2ccb761b5ae7a93
Chakyl <chakyl1127@gmail.com>
2026-07-18 12:26:52 -0400

    1.6-1.21.1

--- src/main/java/top/yourzi/dialog/ui/DialogScreen.java
+++ src/main/java/top/yourzi/dialog/ui/DialogScreen.java
@@ -54,7 +54,17 @@ public class DialogScreen extends Screen {
 
         PortraitDisplayData(String path, float brightness, PortraitPosition position, PortraitAnimationType animationType) {
             if (path != null && !path.isEmpty()) {
-                this.resourceLocation = ResourceLocation.fromNamespaceAndPath(Dialog.MODID, String.format("textures/portraits/%s", path));
+                String namespace = Dialog.MODID;
+                String finalPath = path;
+                if (path.contains(":")) {
+                    String[] split = path.split(":", 2);
+                    namespace = split[0];
+                    finalPath = split[1];
+                } else {
+                    finalPath = String.format("textures/portraits/%s", finalPath);
+                }
+                this.resourceLocation = ResourceLocation.fromNamespaceAndPath(namespace, finalPath);
+
                 this.brightness = brightness;
                 this.position = position != null ? position : PortraitPosition.RIGHT; // 位置
                 this.animationType = animationType != null ? animationType : PortraitAnimationType.NONE; // 动画类型
